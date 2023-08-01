3888dce35fd3edbc9fc6ae6d3c3108933fc4268d
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-01 07:00:39 -0300

    Update relay.js

--- src/core/relay.js
+++ src/core/relay.js
@@ -1,18 +1,39 @@
-import
-{
-{verifySignature, validateEvent, type Event},
-{matchFilters, type Filter},
-{getHex64, getSubscriptionId},
-{ MessageQueue },
-from
-('nostr-tools');
+import {
+  verifySignature,
+  validateEvent,
+  type Event,
+  matchFilters,
+  type Filter,
+  getHex64,
+  getSubscriptionId,
+  MessageQueue,
+} from 'nostr-tools';
 
 const event = {
-  
+  // Define the event properties here
+  // For example:
+  eventId: 'event123',
+  timestamp: Date.now(),
+  payload: { data: 'example data' },
+  // ...
+};
 
-const filter
+const filter = {
+  // Define the filter properties here
+  // For example:
+  filterId: 'filter456',
+  eventType: 'someEventType',
+  // ...
+};
 
 const relay = {
+  // Define the relay properties here
+  // For example:
+  relayId: 'relay789',
+  endpoint: 'https://example.com/relay-endpoint',
+  // ...
+};
+
   
    
 
