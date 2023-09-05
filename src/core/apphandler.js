d68ab73f97870fa6d8ee90ba19d4d20232264087
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-09-05 07:47:04 -0300

    Update and rename handler.js to apphandler.js

---
+++ src/core/apphandler.js
@@ -0,0 +1,17 @@
+import {
+  base64,
+  Event,
+  EventTemplate,
+  Kind,
+  VerifySignature,
+  kind:31990,
+  
+} from '@nostr-dev-kit/ndk';
+
+const nostr = require('ndk');
+const nostr = require('relay');
+const nostr = require('event');
+const nostr = require('handler');
+const nostr = require('secretKey');
+
+export class appHandler
